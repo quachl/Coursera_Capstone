323c957aee40561430cef9f440f183774f758c44
Loc Quach <thlocq@gmail.co>
2019-10-24 23:27:35 -0400

    adds a Notebook to Github

---
+++ Capstone_Project_Notebook_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the Coursera Applied Data Science Capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Project Course")
+
+
+# In[ ]:
+
+
+
+
